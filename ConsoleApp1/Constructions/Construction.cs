10256ba37f7fd021c9d7642426452a06e848d90e
Oleksandra Yevtushenko <w67973@student.wsiz.edu.pl>
2023-10-19 15:49:28 +0200

    meow lab2

---
+++ ConsoleApp1/Constructions/Construction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entrances, int humancapacity, string buildmaterial) //ctor x2tab 
+        {
+            Height = height;
+            Width = width;
+            Entrances = entrances;
+            HumanCapacity = humancapacity;
+            BuildMaterial = buildmaterial;
+
+        }
+
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleApp1.Constructions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(13, 45, 4, 6, "brick");
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
